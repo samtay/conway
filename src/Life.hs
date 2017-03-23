c0db258ceb5365498126cdf1426d36537cb9985f
Sam Tay <sam.chong.tay@gmail.com>
2017-03-23 02:22:09 -0400

    Moving from "grid" to custom comonads, WIP

--- src/Life.hs
+++ src/Life.hs
@@ -8,118 +8,159 @@
 -- Portability :  portable
 --
 -----------------------------------------------------------------------------
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 module Life
   (
   -- * Types
     Board
   , Cell
   , St(..)
+  , Zipper(..)
+  , Z(..) , ZDirection(..)-- todo remove ?
   -- * Construction
   , board
-  , expand
-  , contract
+  , resize
   -- * Running the game
   , step
   , population
   , gameover
   ) where
 
-import Data.List (intersperse)
+import qualified Data.Foldable as F
+import Data.List (nub)
+import Data.Maybe (mapMaybe)
 
-import Math.Geometry.Grid
-  ( Index(..)
-  , neighbours
-  , size
-  )
-import Math.Geometry.GridInternal
-  ( WrappedGrid(..)
-  , normalise
-  )
-import Math.Geometry.Grid.Octagonal
-  ( TorOctGrid(..)
-  , torOctGrid
-  )
-import qualified Math.Geometry.GridMap as GM
-import Math.Geometry.GridMap.Lazy
-  ( LGridMap
-  , lazyGridMap
-  , lazyGridMapIndexed
-  )
+import Data.Sequence (ViewL(..), ViewR(..), (|>), (<|), (><))
+import qualified Data.Sequence as S
+import Control.Comonad
+import Lens.Micro.TH
 
 -- | A modular game of life board
 --
 -- With this interpretation, for a board of size @n x n@
 -- the @(n + 1)@th column/row is the same as the boundary at the @1@th column/row.
-type Board = LGridMap TorOctGrid St
-type Cell = Index Board
+type Board = ZZ St
+
+-- | Indexer for the 'Board'
+type Cell = (Int, Int)
 
 -- | Possible cell states
 data St = Alive | Dead
   deriving (Eq, Show)
+
+-- | Class for a modular bounded container
+--
+-- Examples of functions provided for a simple one dimensional list, where appropriate
+class Zipper z where
+  type Index z
+  type Direction z
+
+  -- | Shift in a direction
+  shift :: Direction z -> z a -> z a
+
+  -- | Retrieve current cursor value
+  cursor :: z a -> a
+
+  -- | Retrieve current index value
+  index :: z a -> Index z
+
+  -- | Retrieve neighborhood of current cursor.
+  neighborhood :: z a -> [a]
+
+  -- | Destruct to list maintaining order of @(Index z)@, e.g. @(Z ls c rs) -> ls ++ [c] ++ rs@.
+  toList :: z a -> [a]
+
+  -- | Like 'toList' but as a mapping with indices.
+  toMap :: z a -> [(Index z, a)]
+
+  -- | Construct zipper from list with starting cursor position, e.g. @(x:xs) -> Z [] x xs@.
+  fromList :: [a] -> z a
+
+  -- | Lookup by possibly denormalised index (still safe from modularity).
+  --
+  -- e.g. [1,2] ! 2 == 1
+  (!) :: z a -> (Index z) -> a
+
+  -- | Normalize @Index z@ value with respect to modular boundaries
+  normalize :: z a -> (Index z) -> (Index z)
+
+  -- | Get size (maximum of @Index z@).
+  size :: z a -> (Index z)
+
+-- | One dimensional finite list with cursor context
+--
+-- The first element of the sequence at '_zl' can be thought of
+-- as /to the left/ of the cursor, while the last element is /to the right/
+-- of the cursor. The cursor value and index are '_zc' and '_zi' respectively.
+-- This can be thought of as a circle.
+-- Warning: must have length greater than zero!
+data Z a = Z { _zl :: S.Seq a
+             , _zc :: a
+             , _zi :: Int
+             } deriving (Show)
+
+data ZDirection = L | R
+  deriving (Eq, Show)
+
+newtype ZZ a = ZZ { unZZ :: Z (Z a) }
+
+makeLenses ''Z
+
+instance Zipper Z where
+  type Index Z = Int
+  type Direction Z = ZDirection
+  shift d z@(Z l c i)
+    | S.length l == 0 = z -- shifting length zero amounts to nothing
+    | d == L          = let (x :< xs) = S.viewl l
+                            ni        = (i - 1) `mod` size z
+                         in Z (xs |> c) x ni
+    | d == R          = let (xs :> x) = S.viewr l
+                            ni        = (i + 1) `mod` size z
+                         in Z (c <| xs) x ni
+  cursor = _zc
+  index = _zi
+  neighborhood (Z l _ _) = mapMaybe (lookupS l) . nub $ [0, S.length l - 1]
+  toList (Z l c i) = F.toList . S.reverse $ b >< (c <| f)
+    where (f,b) = S.splitAt i l
+  toMap = zip [0..] . toList
+  fromList []     = error "Zipper must have length greater than zero."
+  fromList (x:xs) = Z (S.reverse $ S.fromList xs) x 0
+  normalize z = (`mod` (size z))
+  size (Z l _ _) = S.length l + 1
+  z ! k = fnd z (normalize z k)
+    where fnd (Z l c i) n
+            | i == n    = c
+            | i < n     = S.index l $ (s - (n - i) - 1)
+            | i > n     = S.index l $ n - i - 1
+          s = size z
+
+
+instance Functor Z where
+  fmap f (Z l c i) = Z (fmap f l) (f c) i
+
+instance Comonad Z where
+  extract (Z _ c _)  = c
+  extend f z@(Z l _ i) = undefined
 
 -- | Create a board with given height, length, and initial state
 board :: Int -- ^ Height
       -> Int -- ^ Length
       -> [Cell] -- ^ List of cells initially alive
       -> Board
-board h l =
-  foldr
-    (\c g -> GM.insert (normalise g c) Alive g)
-    (lazyGridMap (torOctGrid h l) (repeat Dead))
+board h l = undefined
 
--- | Expand a board by specified number of rows/columns in north/east/south/west directions
-expand :: Int -- ^ Top
-       -> Int -- ^ Right
-       -> Int -- ^ Bottom
-       -> Int -- ^ Left
-       -> Board -- ^ Board to expand
-       -> Board
-expand n e s w b = lazyGridMapIndexed (torOctGrid nh nl) vs
-  where (h,l)                = size b
-        (nh, nl)             = (h + n + s, l + e + w)
-        vs                   = map translate . addRs . addCs $ GM.toList b
-        addCs                = (++) [((x+l,y), Dead) | x <- [0..e + w], y <- [0..nh - 1]]
-        addRs                = (++) [((x,y+h), Dead) | x <- [0..nl - 1], y <- [0..n + s]]
-        translate ((x,y), v) = let nx = (x + w) `mod` nl
-                                   ny = (y + s) `mod` nh
-                                in ((nx, ny), v)
-
-contract :: Int -- ^ Top
-         -> Int -- ^ Right
-         -> Int -- ^ Bottom
-         -> Int -- ^ Left
-         -> Board -- ^ Board to contract
-         -> Board
-contract n e s w b = lazyGridMapIndexed (torOctGrid nh nl) vs
-  where (h,l) = size b
-        (nh, nl) = (h - n - s, l - e - w)
-        vs = map translate . filter fits $ GM.toList b
-        fits ((x,y), _) = and [x >= w, x < l - e, y >= s, y < h - n]
-        translate ((x,y), v) = let nx = (x - w) `mod` nl
-                                   ny = (y - s) `mod` nh
-                                in ((nx, ny), v)
+-- | Adjusts the number of columns and rows respectively.
+--
+-- For example @resize (-1) 1@ will remove one column and add one row.
+resize = undefined
 
 step :: Board -> Board
-step b = GM.mapWithKey rule b
-  where rule :: Cell -> St -> St
-        rule c Dead
-          | liveNeighbors c == 3 = Alive
-          | otherwise            = Dead
-        rule c Alive
-          | liveNeighbors c == 2 = Alive
-          | liveNeighbors c == 3 = Alive
-          | otherwise            = Dead
-
-        liveNeighbors :: Cell -> Int
-        liveNeighbors c = population $ GM.filterWithKey (const . (`elem` neighbours b c)) $ b
+step b = undefined
 
 -- | Returns the total number of living cells in a board
 population :: Board -> Int
-population = sum . map fn . GM.elems
-  where fn Alive = 1
-        fn Dead  = 0
+population = undefined -- length . filter (==Alive) . ...
 
 -- | Check if every cell is dead (i.e., gameover)
 gameover :: Board -> Bool
@@ -130,6 +171,7 @@ gameover = (== 0) . population
 -- Nice when sanity checking or playing in the REPL
 -- TODO this is obviously bad issuing a lookup for each cell
 -- Use toList and then order it into list of type :: [St]
+{-
 instance {-# OVERLAPPING #-} Show Board where
   show b = unlines . reverse $ map mkRow [0..rowT - 1]
     where (rowT, colT)      = size b
@@ -144,3 +186,9 @@ instance {-# OVERLAPPING #-} Show Board where
           toX_ :: Maybe St -> Char
           toX_ (Just Alive) = 'X'
           toX_ _            = '_'
+          -}
+
+lookupS :: S.Seq a -> Int -> Maybe a
+lookupS s n
+  | 0 <= n && n < S.length s = Just $ S.index s n
+  | otherwise                = Nothing
